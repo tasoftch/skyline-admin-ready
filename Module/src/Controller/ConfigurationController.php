718cdcaae70a549424e9b69db29d2db9b39f42e3
Thomas Abplanalp <info@tasoft.ch>
2020-04-29 13:50:36 +0200

    FIX: configuration panel

--- Module/src/Controller/ConfigurationController.php
+++ Module/src/Controller/ConfigurationController.php
@@ -2,9 +2,14 @@
 namespace Skyline\Admin\Ready\Controller;
 
 
+use Skyline\CMS\Security\Exception\CSRFMissmatchException;
 use Skyline\Kernel\Exception\SkylineKernelDetailedException;
+use Skyline\Security\CSRF\CSRFToken;
+use Skyline\Security\CSRF\CSRFTokenManager;
 use Skyline\Security\Exception\SecurityException;
+use Skyline\Translation\TranslationManager;
 use Symfony\Component\HttpFoundation\Request;
+use TASoft\Service\ServiceManager;
 use TASoft\Util\PDO;
 
 class ConfigurationController extends AbstractGeneralAdminController
@@ -36,10 +41,89 @@ class ConfigurationController extends AbstractGeneralAdminController
 			$e->setDetails("For security reasons the configuration can not be ran on a production server. Skyline CMS only accepts the configuration request, if the host's name is localhost or 127.0.0.1\n\nIf you got Skyline CMS directly as installer, you need to reconfigure the installer.");
 			throw $e;
 		}
+		/** @var CSRFTokenManager $csrf */
+		$csrf = $this->CSRFManager;
+		$sm = ServiceManager::generalServiceManager();
 
-		$this->renderTitle("Skyline :: Ready :: Configuration");
-		$this->renderDescription("Adjust initial configuration to be able to launch Skyline CMS Administration panel.");
-		
+		$validParameterKeys = array_keys( require SkyGetPath("$(C)/parameters.config.php"));
+
+		if($request->request->has("updateDB")) {
+			$token = $csrf->getToken("skyline-config-csrf");
+			if(!$csrf->isTokenValid( new CSRFToken("skyline-config-csrf", $request->request->get("skyline-config-csrf")) )) {
+				throw new CSRFMissmatchException("CSRF token missmatch!", 403);
+			}
+
+			$addon = [];
+			if($path = SkyGetPath("$(C)/parameters.addon.config.php")) {
+				$addon = require $path;
+			}
+
+			foreach($request->request as $key => $value) {
+				$key = str_replace('_', '.', $key);
+				if(in_array($key, $validParameterKeys) && $value)
+					$addon[$key] = $value;
+			}
+
+			file_put_contents($path, "<?php\nreturn " . var_export($addon, true) . ";");
+			header("Location: " . $_SERVER["REQUEST_URI"]);
+			exit();
+		}
+
+		/** @var TranslationManager $tm */
+		$tm = $this->translationManager;
+		$tm->setDefaultGlobalTableName("configuration");
+
+		$this->renderTitle("Skyline :: Ready :: " . $tm->translate("Configuration"));
+		$this->renderDescription($tm->translate("Adjust initial configuration to be able to launch Skyline CMS Administration panel."));
+
+		$model = [];
+
+		$model["CSRF"] = $csrf->getToken("skyline-config-csrf");
+
+		$model["PDO"]["primary"] = $sm->getParameter("pdo.primary");
+		$model["PDO"]["secondary"] = $sm->getParameter("pdo.secondary");
+		$model["PDO"]["prefix"] = $prefix = $sm->getParameter("pdo.prefix");
+		$model["PDO"]["sqlite_filename"] = $sm->getParameter("pdo.sqlite.filename");
+		$model["PDO"]["mysql_db"] = $sm->getParameter("pdo.mysql.dataBase");
+		$model["PDO"]["mysql_username"] = $sm->getParameter("pdo.mysql.username");
+		$model["PDO"]["mysql_pass"] = $sm->getParameter("pdo.mysql.password") ? 1 : 0;
+		$model["PDO"]["sqlite_ok"] = is_file($sm->getParameter("pdo.sqlite.filename"));
+
+		$model["CNT"]["ok"] = false;
+
+		try {
+			/** @var PDO $PDO */
+			$PDO = $sm->get("MySQL");
+			$model["PDO"]["mysql_ok"] = true;
+
+			$dbs = [];
+			foreach($PDO->select("SHOW DATABASES") as $db) {
+				$dbs[] = end($db);
+			}
+
+			$model["PDO"]["mysql_dbs"] = $dbs;
+			$model["CNT"]["ok"] = false;
+
+			foreach($PDO->select("SHOW TABLES") as $table) {
+				$table = end($table);
+				if($table == "{$prefix}USER") {
+					$model["CNT"]["ok"] = true;
+					break;
+				}
+			}
+		} catch (\Throwable $e) {
+			$model["PDO"]["mysql_ok"] = false;
+		}
+
+		try {
+			$model["PDO"]["instance"] = $this->PDO;
+		} catch (\Throwable $e) {
+			$model["PDO"]["instance"] = false;
+		}
+
+		$model["PDO"]["ok"] = $model["PDO"]["sqlite_ok"] && $model["PDO"]["mysql_ok"];
+
+		$this->renderModel($model);
 		$this->renderTemplate("admin-main", [
 			"Content" => 'configuration'
 		]);
